ccd76322a290092fff79736bf7096a20bee5e063
adibro500 <ammanabrolu.aditya@gmail.com>
2021-01-23 13:13:10 +0530

    PVT flow only

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ io.on("connection", function (socket) {
   console.log("connected");
   socket.on("join room", (data) => {
     console.log('in room');
-    let Newuser = joinUser(socket.id, data.username, data.roomName, 0, -1, true, false)
+    let Newuser = joinUser(socket.id, data.username, data.roomName, 0, 0, true, false)
     io.to(Newuser.roomname).emit('send data', { username: Newuser.username, roomname: Newuser.roomname, id: socket.id })
     //  io.to(socket.id).emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
     //  socket.emit('send data' , {id : socket.id ,username:Newuser.username, roomname : Newuser.roomname });
@@ -30,10 +30,10 @@ io.on("connection", function (socket) {
     let dat = '';
     console.log('value --- ', data);
 
-   if (data.value == 0) {
+   if (data.value == 0 || thisRoom.isInit) {
      thisRoom.counter = 0;
    }
-   if (data.value == 1) {
+   if (data.value == 1 && !thisRoom.isInit) {
      thisRoom.counter++;
    }
 
@@ -42,34 +42,42 @@ io.on("connection", function (socket) {
     switch (thisRoom.counter) {
       case -1:
         dat = `
-Please Select one of the demo chat Bots:
-1. E Paper
-2. PVT Election Bot
-      `;
-      thisRoom.counter = 0;
-      thisRoom.isInit = true;
-      thisRoom.nextMenu = false;
+      Welcome to PVT 2021
+Please choose one option below:
+{ Its a test version,
+   Enter 1 instead of station no.
+}
+1) Enter Result
+2) Check Result
+3) Upload image of the results
+4) Send image of the QR code
+
+
+Press 0 to go to Main Menu.
+      `; 
+      
         break;
 
         case 0:
-        dat = `
-Please Select one of the demo chat Bots:
-1. E Paper
-2. PVT Election Bot
-      `;
-      thisRoom.counter = 0;
-      thisRoom.isInit = true;
-      thisRoom.nextMenu = false;
+          dat = `
+          Welcome to PVT 2021
+    Please choose one option below:
+    { Its a test version,
+       Enter 1 instead of station no.
+    }
+    1) Enter Result
+    2) Check Result
+    3) Upload image of the results
+    4) Send image of the QR code
+    
+    
+    Press 0 to go to Main Menu.
+          `; 
+     
         break;
 
 case 1:
-  if(!thisRoom.nextMenu) {
-  dat = `
-  Welcome to Daily Nation E-Paper 
-Please enter your Name:
-( It is just a test version. 
-Enter 1 instead of name)
-  `; } else {
+ 
     dat = `
     Please enter polling station no:
     { Its a test version,
@@ -79,28 +87,12 @@ Enter 1 instead of name)
     
     Press 0 to go to Main Menu.
   `;
-  }
+ 
   break;
 
       case 2:
 
-        if(!thisRoom.nextMenu) {
-        dat = `
-#enter province# 
-Please enter your Province:
-( It is just a test version. 
-Enter 1 instead of name)
-1. Lusaka Province
-2. Central Province
-3. Muchinga Province
-4. Copperbelt Province
-5. Eastern Province
-6. Western Provinces
-7. Southern Province
-8. North-Western Province
-9. Luapula Province
-10. Northern Province
-`; } else {
+       
   dat = `
   Northmead Basic School polling
 Please choose one of the option below:
@@ -115,18 +107,9 @@ Please choose one of the option below:
 
 Press 0 to go to Main Menu.
   `;
-}
         break;
       case 3:
-        if(!thisRoom.nextMenu) {
-        dat = `
-#Category#
-We Welcome you to Daily Nation Family. 
-Please tell us what are you looking for: 
-1) Subscribe for Daily Newspaper 
-2) Buy Archives
-3) Get Credential for Website login
-`; }   else {
+      
   dat = `
           Please select political party:
 { Its a test version,
@@ -142,20 +125,9 @@ Please tell us what are you looking for:
 
 Press 0 to go to Main Menu.
           `
-}
         break;
       case 4:
-        if(!thisRoom.nextMenu) {
-        dat = `#Subscribe for Daily Newspaper#
-Please select the subscription duration:
-1) 1 Day Subscription
-2) 7 Days Subscription
-3) 14 Days Subscription
-4) 28 Days Subscription
-5) 3 Months Subscription
-6) 6 Months Subscription
-7) 1 year Subscription`
-        }  else {
+      
           dat = `
           Please enter the Vote counts:
         { Its a test version,
@@ -165,28 +137,16 @@ Please select the subscription duration:
         
         Press 0 to go to Main Menu.
           `; 
-        }
+        
         break;
-      case 5:
-        if(!thisRoom.nextMenu) { 
-        dat = `
-#subscription pay#
-You selected 1 Day Subscription.
-Please go to the link below to pay K5. As soon as payment gets completed you will be redirected to your dashboard containing the Newspapers you have bought.
-
-https://flutterwave.com/us/
-
-
-Press 0 to go to Main Menu.
-`; } else {
+  case  5:  
   dat = `
   #New Status#
 Updated Total Vote Count in presidential election for the PF political party is : 101
 
 
 Press 0 to go to the Main Menu
-  `
-}  
+  ` 
         break;
 
 default: 
@@ -207,24 +167,7 @@ break;
       `;
      }
 
-     if ( (data.value == 2) && thisRoom.isInit && !thisRoom.nextMenu) {
-      thisRoom.nextMenu = true
-      dat = `
-      Welcome to PVT 2021
-Please choose one option below:
-{ Its a test version,
-   Enter 1 instead of station no.
-}
-1) Enter Result
-2) Check Result
-3) Upload image of the results
-4) Send image of the QR code
-
-
-Press 0 to go to Main Menu.
-      `; 
-      thisRoom.counter = 0;
-    }
+   
 
 
     console.log('counter ====', thisRoom.counter);
